b7045c0cfcc26d37cbec315d699facb315bbc6df
basil832025 <1245640@gmail.com>
2026-07-13 19:31:30 +0300

    Flatten package view structure

--- src/FrontendThreePirogaServiceProvider.php
+++ src/FrontendThreePirogaServiceProvider.php
@@ -3,14 +3,17 @@
 namespace Basil832025\FrontendThreePiroga;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class FrontendThreePirogaServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->app['view']->addLocation(__DIR__ . '/../resources/views');
-        Blade::anonymousComponentPath(__DIR__ . '/../resources/views/front/3piroga/components');
+        $viewPath = __DIR__ . '/../resources/views';
+
+        View::addNamespace('front.3piroga', $viewPath);
+        Blade::anonymousComponentPath($viewPath . '/components');
 
         $this->publishes([
             __DIR__ . '/../public' => public_path(),
